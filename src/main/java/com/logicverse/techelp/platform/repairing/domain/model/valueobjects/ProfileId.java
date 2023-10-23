4b801f00a6a332cdbeefc124158228036b76be27
GioRC0 <142363773+GioRC0@users.noreply.github.com>
2023-10-23 17:15:56 -0500

    chore: added dependencies.

---
+++ src/main/java/com/logicverse/techelp/platform/repairing/domain/model/valueobjects/ProfileId.java
@@ -0,0 +1,14 @@
+package com.logicverse.techelp.platform.repairing.domain.model.valueobjects;
+
+import jakarta.persistence.Embeddable;
+
+@Embeddable
+public record ProfileId(Long profileId) {
+    public ProfileId(){
+        this(0L);
+    }
+
+    public ProfileId{
+        if(profileId < 0) throw new IllegalArgumentException("Profile Id cannot be negative");
+    }
+}
