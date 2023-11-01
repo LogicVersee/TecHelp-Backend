c3bfc367367eb794b8193f1919d0ea8ebd4d3369
Mirroy25 <112901601+Mirroy25@users.noreply.github.com>
2023-11-01 18:39:52 -0500

    feat(task): task entity created

---
+++ src/main/java/com/logicverse/techelp/platform/tasking/domain/model/entities/Task.java
@@ -0,0 +1,66 @@
+package com.logicverse.techelp.platform.tasking.domain.model.entities;
+import com.logicverse.techelp.platform.monitoring.domain.model.entities.ComponentItem;
+import com.logicverse.techelp.platform.repairing.domain.model.valueobjects.EmailAddress;
+import com.logicverse.techelp.platform.repairing.domain.model.valueobjects.PersonName;
+import com.logicverse.techelp.platform.shared.domain.model.entities.AuditableModel;
+import com.logicverse.techelp.platform.tasking.domain.model.valueobjects.ClientName;
+import com.logicverse.techelp.platform.tasking.domain.model.valueobjects.ClientPhoneName;
+import com.logicverse.techelp.platform.tasking.domain.model.valueobjects.ClientProblem;
+import com.logicverse.techelp.platform.tasking.domain.model.valueobjects.DeliveryDay;
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.lang.reflect.Array;
+import java.util.Date;
+import java.util.function.LongFunction;
+
+@Entity
+public class Task extends AuditableModel {
+    @Id
+    @Getter
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Getter
+    private ClientName name;
+
+    @Getter
+    private ClientPhoneName clientphone;
+
+    @Getter
+    private ClientProblem problem;
+
+    @Embedded
+    private String[] componentItem;
+
+    @Getter
+    private DeliveryDay deliveryDay;
+
+    @Getter
+    private String income;
+
+    public Task(String client_name, String client_phone, String problem, String[] component, Date delivery_date, String income){
+        this.componentItem =component;
+        this.name = new ClientName(client_name);
+        this.clientphone = new ClientPhoneName(client_phone);
+        this.deliveryDay = new DeliveryDay(delivery_date);
+        this.problem = new ClientProblem(problem);
+        this.income = income;
+    }
+
+    public Task(){}
+
+    public String getClientName(){
+        return this.name.getClientName();
+    }
+
+    public String getClientPhoneName(){
+        return this.clientphone.getClientPhoneName();
+    }
+
+    public String getClientProblem(){
+        return this.problem.getClientProblem();
+    }
+
+    public  String getDeliveryDay(){ return  this.deliveryDay.getDeliveryDay();}
+}
