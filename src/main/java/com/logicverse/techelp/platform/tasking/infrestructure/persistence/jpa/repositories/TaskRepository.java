a210a72d51d4c7e39c8994c08a6fe467ac2e0344
Mirroy25 <112901601+Mirroy25@users.noreply.github.com>
2023-11-01 19:35:52 -0500

    feat(task): task repository created

---
+++ src/main/java/com/logicverse/techelp/platform/tasking/infrestructure/persistence/jpa/repositories/TaskRepository.java
@@ -0,0 +1,14 @@
+package com.logicverse.techelp.platform.tasking.infrestructure.persistence.jpa.repositories;
+import com.logicverse.techelp.platform.monitoring.domain.model.valueobjects.TechnicianId;
+import com.logicverse.techelp.platform.tasking.domain.model.entities.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task,Long> {
+
+
+    Optional<Task> findByTechnicianId(TechnicianId TechnicianId);
+}
