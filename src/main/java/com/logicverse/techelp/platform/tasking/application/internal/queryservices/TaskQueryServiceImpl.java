b2c45b715a653a7886320fa050109ed7409784f6
proyect <luis.eduardo.200325@gmail.com>
2023-11-02 08:51:39 -0500

    feat(task): task query service implementation created

--- src/main/java/com/logicverse/techelp/platform/tasking/application/internal/queryservices/TaskQueryServiceImpl.java
+++ src/main/java/com/logicverse/techelp/platform/tasking/application/internal/queryservices/TaskQueryServiceImpl.java
@@ -1,4 +1,34 @@
 package com.logicverse.techelp.platform.tasking.application.internal.queryservices;
 
-public class TaskQueryServiceImpl {
+import com.logicverse.techelp.platform.repairing.domain.model.entities.Technical;
+import com.logicverse.techelp.platform.repairing.domain.model.queries.GetTechnicalByEmailQuery;
+import com.logicverse.techelp.platform.repairing.domain.model.queries.GetTechnicalByIdQuery;
+import com.logicverse.techelp.platform.repairing.infrastructure.persistence.jpa.repositories.TechnicalRepository;
+import com.logicverse.techelp.platform.tasking.domain.model.entities.Task;
+import com.logicverse.techelp.platform.tasking.domain.model.queries.GetTaskByIdQuery;
+import com.logicverse.techelp.platform.tasking.domain.services.TaskQueryService;
+import com.logicverse.techelp.platform.tasking.infrestructure.persistence.jpa.repositories.TaskRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+@Service
+public class TaskQueryServiceImpl implements TaskQueryService {
+
+    private TaskRepository taskRepository;
+
+    public TaskQueryServiceImpl(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    @Override
+    public Optional<Task> handle(GetTaskByIdQuery query){
+        return taskRepository.findById(query.taskId());
+    }
+
+    @Override
+    public List<Task> handle(){return taskRepository.findAll();
+    }
+
+
 }
