c21233ed8bf057a3512d1e0d82b359e9c30ff61c
proyect <luis.eduardo.200325@gmail.com>
2023-11-02 08:51:29 -0500

    feat(task): task command service implementation created

--- src/main/java/com/logicverse/techelp/platform/tasking/application/internal/commandservices/TaskCommandServiceImpl.java
+++ src/main/java/com/logicverse/techelp/platform/tasking/application/internal/commandservices/TaskCommandServiceImpl.java
@@ -1,9 +1,15 @@
 package com.logicverse.techelp.platform.tasking.application.internal.commandservices;
 
+import com.logicverse.techelp.platform.monitoring.domain.model.valueobjects.TechnicianId;
 import com.logicverse.techelp.platform.tasking.domain.model.commands.CreateTaskCommand;
+import com.logicverse.techelp.platform.tasking.domain.model.entities.Task;
+import com.logicverse.techelp.platform.tasking.domain.model.queries.GetTaskByIdQuery;
 import com.logicverse.techelp.platform.tasking.domain.services.TaskCommandService;
 import com.logicverse.techelp.platform.tasking.infrestructure.persistence.jpa.repositories.TaskRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TaskCommandServiceImpl implements TaskCommandService {
@@ -15,6 +21,18 @@ public class TaskCommandServiceImpl implements TaskCommandService {
 
     @Override
     public  Long handle(CreateTaskCommand command){
+        var technicianId =new TechnicianId();
+        var task =this.taskRepository.findById(technicianId.technicianId());
 
+        if(task.isEmpty()){
+            var newTask = new Task(command.client_name(),command.client_phone(),command.problem(),
+                    command.component(),command.delivery_date(),command.income());
+            this.taskRepository.save(newTask);
+
+            return newTask.getId();
+        }
+
+        throw new IllegalArgumentException("Task already exists");
     }
+
 }
