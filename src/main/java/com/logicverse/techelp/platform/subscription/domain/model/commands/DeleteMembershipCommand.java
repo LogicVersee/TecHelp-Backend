1d3ffb3b2ea379b5890ef30bf4a075cb9965002a
Yorusito <sebastian1987102@gmail.com>
2023-11-11 21:16:16 -0500

    feat(membership): command services implemented of application.

--- src/main/java/com/logicverse/techelp/platform/subscription/domain/model/commands/DeleteMembershipCommand.java
+++ src/main/java/com/logicverse/techelp/platform/subscription/domain/model/commands/DeleteMembershipCommand.java
@@ -1,4 +1,4 @@
 package com.logicverse.techelp.platform.subscription.domain.model.commands;
 
-public record DeleteMembershipCommand(Long courseId) {
+public record DeleteMembershipCommand(Long membershipId) {
 }
