a0f6b1b3550fd8d9ad6f13a090fbeef52e9aee25
Yorusito <sebastian1987102@gmail.com>
2023-11-20 22:52:06 -0500

    feat(iam): models of domain implemented.

---
+++ src/main/java/com/logicverse/techelp/platform/iam/domain/model/valueobjects/Roles.java
@@ -0,0 +1,10 @@
+package com.logicverse.techelp.platform.iam.domain.model.valueobjects;
+/**
+ * Roles
+ * Enumeration with default Roles
+ */
+public enum Roles {
+    ROLE_USER,
+    ROLE_TECHNICIAN,
+    ROLE_ADMIN
+}
