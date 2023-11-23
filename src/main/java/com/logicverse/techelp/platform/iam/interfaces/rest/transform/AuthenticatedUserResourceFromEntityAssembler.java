fa4077b16170749edcfc1304d13b2270ef797e2c
GioRC0 <142363773+GioRC0@users.noreply.github.com>
2023-11-23 00:10:05 -0500

    feat(iam): added rol param

--- src/main/java/com/logicverse/techelp/platform/iam/interfaces/rest/transform/AuthenticatedUserResourceFromEntityAssembler.java
+++ src/main/java/com/logicverse/techelp/platform/iam/interfaces/rest/transform/AuthenticatedUserResourceFromEntityAssembler.java
@@ -4,6 +4,6 @@ import com.logicverse.techelp.platform.iam.domain.model.aggregates.User;
 import com.logicverse.techelp.platform.iam.interfaces.rest.resources.AuthenticatedUserResource;
 public class AuthenticatedUserResourceFromEntityAssembler {
     public static AuthenticatedUserResource toResourceFromEntity(User user, String token) {
-        return new AuthenticatedUserResource(user.getId(), user.getUsername(), token);
+        return new AuthenticatedUserResource(user.getId(), user.getUsername(), token, (user.getRoles().iterator().next().getName()).toString());
     }
 }
